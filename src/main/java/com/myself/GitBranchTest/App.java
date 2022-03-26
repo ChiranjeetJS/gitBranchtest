9747b7b3d52f33eca5d6104aa7acd8ad89d9bff9
ChiranjeetJS <90850386+ChiranjeetJS@users.noreply.github.com>
2022-03-26 20:20:23 +0530

    Update App.java
    added some new feature

--- src/main/java/com/myself/GitBranchTest/App.java
+++ src/main/java/com/myself/GitBranchTest/App.java
@@ -10,6 +10,7 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println(" Add some more code");
+        System.out.println("Some more code added");
         
     }
 }
