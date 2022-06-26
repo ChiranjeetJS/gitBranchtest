d2de34a6b36a3630a43749bf03ab43c40e635b0c
ChiranjeetJS <Chiranjeetcjs123@gmail.com>
2022-06-26 10:33:47 +0530

    changed line 12

--- src/main/java/com/myself/GitBranchTest/App.java
+++ src/main/java/com/myself/GitBranchTest/App.java
@@ -9,11 +9,12 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println(" Add some more code");
+        System.out.println(" Add some more code from test2");
         System.out.println("Some more code added");
         System.out.println("Once more");
         someRandomMethod();
         System.out.println("test branch");
+        System.out.println(" test2 branch");
 
         
     }
